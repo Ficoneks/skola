99433550060105ca6ecb7b673bf15777cfdfcb35
Ficoneks <filip.jambrosic09@gmail.com>
2025-01-18 14:41:01 +0100

    Add files via upload

---
+++ vjezba 5/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a;
+    printf("unesi malo slovo:");
+    cin>> a;
+    a = a - 32;
+    printf("Veliko slovo %c", a);
+    return 0;
+
+}
+
