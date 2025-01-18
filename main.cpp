4f74ce331be5bba955cddacd00f34ecd9e1f1208
Ficoneks <filip.jambrosic09@gmail.com>
2025-01-18 14:37:35 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a;
+    printf("unesi neki znak: ");
+    cin>> a;
+    a = a + 1;
+    printf("sljedeci znak je : %c", a);
+    return 0;
+}
