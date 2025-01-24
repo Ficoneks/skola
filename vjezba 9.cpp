c7a34da6aad8dcfa01224402a75f376b6a68810f
Ficoneks <filip.jambrosic09@gmail.com>
2025-01-25 00:11:44 +0100

    Add files via upload

---
+++ vjezba 9.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{     
+	int a;
+	printf("unesi neki broj:");
+	cin >> a;
+	int b;
+	printf("Hvala! unesi drugi broj:");
+	cin >> b;
+	int r = 0;
+	printf("%i + %i = %i\n ", a, b, a + b);
+	printf("%i - %i = %i\n ", a, b, a - b);
+	printf("%i * %i = %i\n ", a, b, a * b);
+	printf("%i / %i = %i\n ", a, b, a / b);
+	return 0;
+
+
+}
+
