a0b74e02dc59ebba3a7b57e8ff597d52eadf4e55
Ficoneks <filip.jambrosic09@gmail.com>
2025-01-27 18:09:51 +0100

    Add files via upload

---
+++ vjz13.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    char a;
+    printf("unesi neki znak:");
+    cin >> a;
+    int c;
+    c = a - 1;
+    int d;
+    d = a + 1;
+    printf(" %c %c", c ,d);
+    return 0;
+    
+}
