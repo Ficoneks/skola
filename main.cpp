bda5c27ef290fc8939e7ea566c780a6bda7095e2
Ficoneks <filip.jambrosic09@gmail.com>
2025-02-07 15:54:03 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,12 +2,43 @@
 
 using namespace std;
 
-int main()
-{
-    char a;
-    printf("unesi znak:");
-    cin>> a;
-    printf("ASCII vrijednost znaka je: %i", a);
-    return 0;
+int main() {
+	char a;
+	int r, i, u;
 
-}
+	cout << "unesi velicinu zelis izracunati:";
+	cin >> a;
+
+	if (a == 'R') {
+		cout << "unesi napon:";
+		cin >> u;
+		cout<<"unesi struju:";
+		cin>>i;
+		int R;
+		R = u/i;
+		cout<<R;
+	}
+
+	else if (a=='U'){
+        cout<<"unesi otpor:";
+        cin>>r;
+        cout<<"unesi  struju:";
+        cin>>i;
+        int U;
+        U = r*i;
+        cout<<U;
+	}
+
+	else if(a == 'I'){
+        cout<<"unesi otpor:";
+        cin>>r;
+        cout<<"unesi napon";
+        cin>>u;
+        int I;
+        I = u / r;
+        cout<<I;
+
+	}
+	return 0;
+
+	}
