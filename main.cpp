d2328d075fc721428e1ad5d6f7de721f4201fcaa
Ficoneks <filip.jambrosic09@gmail.com>
2025-05-28 17:12:45 +0200

    Add files via upload
    shai

--- main.cpp
+++ main.cpp
@@ -1,20 +1,52 @@
 #include <iostream>
 using namespace std;
-
-//napisi program koji ispise koliko je parnih brojeva u polju
+/*
 int main() {
-    int brojac = 0;
-    int a = 1;
-    int b = 10;
-    int polje[10];
-    for ( int i = 0; i<10; i++) {
-        polje[i] = rand() % (b-a+1)+a;
-    }
-    for ( int i = 0; i<10; i++) {
-        if (polje[i] % 2 == 0) {
-            brojac++;
+    int p[3][3];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> p[i][j];
         }
     }
-    cout << brojac << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << p[i][j] << " ";
+
+        }
+        cout << endl;
+    }
     return 0;
+}
+*/
+int main() {
+    char p[100][20];
+    bool bv = 0;
+
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 20; j++) {
+            cout<<"unesi slovo: ";
+            cin>>p[i][j];
+            if (p[i][j] == '0')
+                break;
+            if (p[i][j] == '1' && j == 0) {
+                bv = 1;
+                break;
+            }
+
+        }
+        if (bv == 1)
+            break;
+    }
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 20; j++) {
+            if (p[i][j] == '1'  || p[i][j] == 0) {
+                break;
+            }
+
+            cout<<p[i][j - 1];
+        }
+cout<<endl;
+    }
+    return 0;
+
 }
